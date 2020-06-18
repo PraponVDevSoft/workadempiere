7ec19c154fc8b9614f5124acad30282abd8f35d2
vdevsoft <prapon@live.com>
2020-06-18 14:50:15 +0700

    371

---
+++ db-2-pc.bat
@@ -0,0 +1,2 @@
+xcopy D:\srcBackup\Dropbox\work-luna-371w\*.java  D:\work-luna-371w /s /d /y
+xcopy D:\srcBackup\Dropbox\work-luna-371w\*.bat  D:\work-luna-371w /s /d /y
---
+++ pc-2-db.bat
@@ -0,0 +1,2 @@
+xcopy D:\work-luna-371w D:\srcBackup\Dropbox\work-luna-371w /s /d /y
+pause
